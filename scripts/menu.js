9ee032cf0e93921fd83fdfe70f7b3bc911672465
Vítor Marques de Faria <vtrmrx@gmail.com>
2017-04-24 14:26:25 -0300

    encomendas

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('#icone-menu').click(function(){
     $('#menu-phone').toggleClass('aberto');
+    $('#menu').toggleClass('aberto');
     $('body').toggleClass('fixo');
   });
 });
