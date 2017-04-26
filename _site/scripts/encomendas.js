c5d1648e6ac73939d2e295ae9c2c272ee0cffe99
Vítor Marques de Faria <vtrmrx@gmail.com>
2017-04-26 18:09:32 -0300

    minha conta

---
+++ _site/scripts/encomendas.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$('#novo-local .botao').click(function(){
+		$('#adicionar-endereco').toggleClass('aberto');
+		$('body').toggleClass('fixo');
+	});
+	$('#adicionar-endereco .fundo').click(function(){
+		$('#adicionar-endereco').toggleClass('aberto');
+		$('body').toggleClass('fixo');
+		$('#adicionar-endereco').removeClass('cep-invalido');
+	});
+	$('#adicionar-endereco .fechar').click(function(){
+		$('#adicionar-endereco').toggleClass('aberto');
+		$('body').toggleClass('fixo');
+		$('#adicionar-endereco').removeClass('cep-invalido');
+	});
+});
+
+function validarCep() {
+ 	var x = document.forms["novoLocal"]["cep"].value;
+	if ( (x >= 70000000 && x <= 72799999) || (x >= 73000000 && x <= 73699999) ) {
+		alert("endereço para entrega adicionado");
+		$('#adicionar-endereco').toggleClass('aberto');
+		$('body').toggleClass('fixo');
+		$('#adicionar-endereco').removeClass('cep-invalido');
+		return false;
+	} else {
+	    $('#adicionar-endereco').addClass('cep-invalido');
+	    return false;
+	}
+}
+
+function buscarnaFabrica() {
+ 	$('#adicionar-endereco').toggleClass('aberto');
+	$('body').toggleClass('fixo');
+	$('#adicionar-endereco').removeClass('cep-invalido');
+ 	return false;
+}
